1a88a940c56ec3418061cfa072e0d5ec342b3691
Ajyamkar <ajyamkar99@gmail.com>
2023-07-13 23:46:57 +0530

    auth related apis to new routes folder file

---
+++ routes/Auth.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { usersDB } = require("../config/db");
+const { userAuthentication, verifyToken } = require("../Middlewares/Auth");
+const { generateToken } = require("../utils");
+const router = express.Router();
+
+router.use(express.json());
+
+router.post("/login", userAuthentication, (req, res) => {
+  res.send({
+    message: "LoggedIn successfully",
+    token: generateToken(req.user._id),
+  });
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  if (!email || !password || !firstName || !lastName) {
+    res.status(422).send("Some fields values are missing.");
+    return;
+  }
+  const user = await usersDB.findOne({ email });
+  if (user) {
+    res.status(422).send("User already exists.");
+    return;
+  }
+  try {
+    const newUser = await usersDB({ ...req.body }).save();
+    res.send({
+      message: "Created user successfully.",
+      token: generateToken(newUser._id),
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put("/updatePassword", async (req, res) => {
+  const { email, updatedPassword } = req.body;
+  if (!email || !updatedPassword) {
+    res.status(422).send("Some fields are missing.");
+    return;
+  }
+  const foundUser = await usersDB.findOneAndUpdate(
+    { email },
+    { password: updatedPassword }
+  );
+  if (!foundUser) {
+    res.status(404).send("User not found");
+  } else {
+    foundUser.password = updatedPassword;
+    res.send({
+      message: "Successfully updated the password.",
+      token: generateToken(foundUser._id),
+    });
+  }
+});
+
+router.get("/isUserLoggedIn", verifyToken, (req, res) => {
+  res.send("User is loggedin.");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,73 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const { userAuthentication, verifyToken } = require("./Middlewares/Auth");
-const { connectDatabase, usersDB } = require("./config/db");
-const { generateToken } = require("./utils");
+const { verifyToken } = require("./Middlewares/Auth");
+const { connectDatabase } = require("./config/db");
 const {
   PORT,
   FRONTEND_URL,
   GOOGLE_AUTH_PROMPT_URL,
 } = require("./config/config");
+const authRoute = require("./routes/Auth");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: FRONTEND_URL }));
 connectDatabase();
 
-app.post("/api/login", userAuthentication, (req, res) => {
-  res.send({
-    message: "LoggedIn successfully",
-    token: generateToken(req.user._id),
-  });
-});
-
-app.post("/api/signup", async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
-  if (!email || !password || !firstName || !lastName) {
-    res.status(422).send("Some fields values are missing.");
-    return;
-  }
-  const user = await usersDB.findOne({ email });
-  if (user) {
-    res.status(422).send("User already exists.");
-    return;
-  }
-  try {
-    const newUser = await usersDB({ ...req.body }).save();
-    res.send({
-      message: "Created user successfully.",
-      token: generateToken(newUser._id),
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.put("/api/updatePassword", async (req, res) => {
-  const { email, updatedPassword } = req.body;
-  if (!email || !updatedPassword) {
-    res.status(422).send("Some fields are missing.");
-    return;
-  }
-  const foundUser = await usersDB.findOneAndUpdate(
-    { email },
-    { password: updatedPassword }
-  );
-  if (!foundUser) {
-    res.status(404).send("User not found");
-  } else {
-    foundUser.password = updatedPassword;
-    res.send({
-      message: "Successfully updated the password.",
-      token: generateToken(foundUser._id),
-    });
-  }
-});
-
-app.get("/api/isUserLoggedIn", verifyToken, (req, res) => {
-  res.send("User is loggedin.");
-});
+// Routes
+app.use("/api/auth", authRoute);
 
 app.get("/api/dashboard", verifyToken, (req, res) => {
   res.send({ message: "Dashboard", user: req.user });
@@ -79,10 +28,6 @@ app.get("/", (req, res) => {
 
 app.get("/api/getAuthUrl/google", (req, res) => {
   res.send(GOOGLE_AUTH_PROMPT_URL);
-});
-
-app.get("/api/googleAuthRedirect", (req, res) => {
-  console.log(req.url);
 });
 
 app.listen(PORT, () => {
