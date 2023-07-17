99d814ecaca28e0030c2c38ef42a9c096fe9fa20
Ajyamkar <ajyamkar99@gmail.com>
2023-07-17 23:45:28 +0530

    Added google auth signin api without storing user data

--- config/config.js
+++ config/config.js
@@ -1,10 +1,17 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_AUTH_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_REDIRECT_URI = process.env.GOOGLE_AUTH_REDIRECT_URL;
+
 module.exports = {
   MONGO_URL: process.env.MONGO_URL,
   SECRET_KEY: process.env.SECRET,
   PORT: process.env.PORT,
   FRONTEND_URL: process.env.FRONTEND_URL,
-  GOOGLE_AUTH_PROMPT_URL: `${process.env.GOOGLE_AUTH_BASE_URL}?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_AUTH_REDIRECT_URL}&response_type=token&scope=${process.env.GOOGLE_AUTH_SCOPE}&state=${process.env.GOOGLE_AUTH_STATE}&prompt=consent`,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_URI,
+  GOOGLE_AUTH_PROMPT_URL: `${process.env.GOOGLE_AUTH_BASE_URL}?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_AUTH_REDIRECT_URL}&response_type=code&scope=${process.env.GOOGLE_AUTH_SCOPE}&state=${process.env.GOOGLE_AUTH_STATE}&prompt=consent`,
 };
--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const {
   PORT,
   FRONTEND_URL,
   GOOGLE_AUTH_PROMPT_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_URI,
 } = require("./config/config");
 const authRoute = require("./routes/Auth");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -28,6 +32,32 @@ app.get("/", (req, res) => {
 
 app.get("/api/getAuthUrl/google", (req, res) => {
   res.send(GOOGLE_AUTH_PROMPT_URL);
+});
+
+app.post("/api/auth/googleSignIn", async (req, res) => {
+  const { code } = req.body;
+  const url = `https://oauth2.googleapis.com/token?client_id=${GOOGLE_CLIENT_ID}&client_secret=${GOOGLE_CLIENT_SECRET}&code=${code}&redirect_uri=${GOOGLE_REDIRECT_URI}&grant_type=authorization_code`;
+  try {
+    const response = await axios.post(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const data = response.data;
+    const { access_token } = data;
+    axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`
+      )
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        res.status(401).send(err);
+      });
+  } catch (error) {
+    res.status(401).send(error.response.data);
+  }
 });
 
 app.listen(PORT, () => {
